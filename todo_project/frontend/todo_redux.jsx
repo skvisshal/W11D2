b169cc6b2b3434322d3c0c01a8bf5d26aa66a96c
Visshal Suresh <skvisshal@gmail.com>
2021-12-14 12:19:22 -0500

    initial setup

---
+++ todo_project/frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDOM.render(document.getElementById('main'));
+});
