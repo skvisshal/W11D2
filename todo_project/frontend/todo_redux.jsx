a2c7259f96843c734bcf045a497a1653396dca65
Anuj Gupta <apgupta3091@gmail.com>
2021-12-14 15:25:38 -0500

    finish todo_project p1

--- todo_project/frontend/todo_redux.jsx
+++ todo_project/frontend/todo_redux.jsx
@@ -3,31 +3,55 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
 import { RECEIVE_TODO, RECEIVE_TODOS, REMOVE_TODO, receiveTodo, receiveTodos, removeTodo } from './actions/todo_actions';
+import { RECEIVE_STEP, RECEIVE_STEPS, REMOVE_STEP, receiveStep, receiveSteps, removeStep } from './actions/step_actions';
 
-const newTodos = {
+const items = {
+  todos: {
     1: {
-        id: 1,
-        title: "wash car",
-        body: "with soap",
-        done: false
-    },
-    2: {
-        id: 2,
-        title: "wash dog",
-        body: "with shampoo",
-        done: true
+      id: 1,
+      title: 'take a shower',
+      body: 'and be clean',
+      done: false
     }
-};
+  },
+  steps: {
+    1: { // this is the step with id = 1
+      id: 1,
+      title: 'walk to store',
+      done: false,
+      todo_id: 1
+    },
+    2: { // this is the step with id = 2
+      id: 2,
+      title: 'buy soap',
+      done: false,
+      todo_id: 1
+    }
+  }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     const store = configureStore();
     window.store = store;
-    console.log(store.getState());
-    console.log(store.dispatch(receiveTodo({ id: 3, title: "New Todo" })));
-    console.log(store.getState());
-    console.log(store.dispatch(receiveTodos(newTodos)));
-    console.log(store.getState());
-    console.log(store.dispatch(removeTodo(newTodos[2])));
-    console.log(store.getState());
+    const newTodos = [{ id: 1, title: 'Learn Redux', body: 'It is fundamental', done: false }  ];
+
+    const newSteps = [{ id: 1, title: 'Dispatch actions', done: false, todo_id: 1 }];
+
+    store.dispatch(receiveTodos(newTodos));
+    store.dispatch(receiveSteps(newSteps));
+    console.log(store.getState()); // should return only the new todos and steps
+
+    store.dispatch(receiveTodo({ id: 3, title: "New Todo"}));
+    store.dispatch(receiveStep({ id: 3, title: "New Step"}));
+    console.log(store.getState()); // should include the newly added todo and step
+
+    store.dispatch(receiveTodo({ id: 3, title: "Newer Todo"}));
+    store.dispatch(receiveStep({ id: 3, title: "Newer Step", done: true}));
+    console.log(store.getState()); // should update the previously added todo and step
+
+    store.dispatch(removeTodo({ id: 3, title: "Newer Todo" }));
+    store.dispatch(removeStep({ id: 3, title: "Newer Step"}));
+    console.log(store.getState()); // should not include the previously added todo
+    
     // ReactDOM.render(store, document.getElementById('main'));
 });
