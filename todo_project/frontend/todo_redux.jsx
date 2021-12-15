97589a7d36e58bd97b1c8481fe64177934fc37f6
Anuj Gupta <apgupta3091@gmail.com>
2021-12-15 14:03:41 -0500

    finish phase1 of todos part 2

--- todo_project/frontend/todo_redux.jsx
+++ todo_project/frontend/todo_redux.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import App from './components/app';
+import Root from './components/root';
 
 import { RECEIVE_TODO, RECEIVE_TODOS, REMOVE_TODO, receiveTodo, receiveTodos, removeTodo } from './actions/todo_actions';
 import { RECEIVE_STEP, RECEIVE_STEPS, REMOVE_STEP, receiveStep, receiveSteps, removeStep } from './actions/step_actions';
@@ -33,25 +35,5 @@ const items = {
 document.addEventListener('DOMContentLoaded', function () {
     const store = configureStore();
     window.store = store;
-    const newTodos = [{ id: 1, title: 'Learn Redux', body: 'It is fundamental', done: false }  ];
-
-    const newSteps = [{ id: 1, title: 'Dispatch actions', done: false, todo_id: 1 }];
-
-    store.dispatch(receiveTodos(newTodos));
-    store.dispatch(receiveSteps(newSteps));
-    console.log(store.getState()); // should return only the new todos and steps
-
-    store.dispatch(receiveTodo({ id: 3, title: "New Todo"}));
-    store.dispatch(receiveStep({ id: 3, title: "New Step"}));
-    console.log(store.getState()); // should include the newly added todo and step
-
-    store.dispatch(receiveTodo({ id: 3, title: "Newer Todo"}));
-    store.dispatch(receiveStep({ id: 3, title: "Newer Step", done: true}));
-    console.log(store.getState()); // should update the previously added todo and step
-
-    store.dispatch(removeTodo({ id: 3, title: "Newer Todo" }));
-    store.dispatch(removeStep({ id: 3, title: "Newer Step"}));
-    console.log(store.getState()); // should not include the previously added todo
-    
-    // ReactDOM.render(store, document.getElementById('main'));
+    ReactDOM.render(<Root store={store}/>, document.getElementById('content'));
 });
